d173b2f249049c88bbee651961d204e8b9db8a7e
exorix <locherthe@web.de>
2023-05-22 02:39:02 +0200

    Tolles Diagramm + Settings

---
+++ ArchitectureDiagramm.gd
@@ -0,0 +1,48 @@
+
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var spalte = $Spalte
+onready var grid = $Grid
+onready var drawer = $Drawer
+export(int)var Spaltenanzahl = 6;
+export(Array,int) var anzahlProSpalte = [51,5,3,9,2,11]
+ 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in Spaltenanzahl:
+		var newSpalte = spalte.duplicate()
+		print(newSpalte)
+#		var parent = newSpalte.get_parent()
+#		print(parent)
+#		parent.remove_child(newSpalte);
+		grid.add_child(newSpalte)
+
+		if Spaltenanzahl > anzahlProSpalte.size():
+			for j in Spaltenanzahl - anzahlProSpalte.size():
+				anzahlProSpalte.append(1)
+		for j in anzahlProSpalte[i]:
+			if j <11:
+				newSpalte.get_child(j).visible = true
+				print(newSpalte.get_child(j).rect_global_position)
+			else:
+				break
+	for i in grid.get_child_count():
+		print(grid.get_child(i))
+	$Timer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+func _draw():
+	pass
+
+func inputchange(inputs,outputs):
+	pass
+
+func _on_Timer_timeout():
+	drawer.setDrawPoints(Spaltenanzahl,anzahlProSpalte)
---
+++ Settings.gd
@@ -0,0 +1,108 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var anzahlHiddenlayer = 0
+var anzahlInputs = 1
+var anzahlOutputs = 1
+var neuronenProHiddenlayer = [1,0]
+var inputs = []
+var outputs = []
+onready var inputContainer = $VBoxContainer/HBoxContainer4/VBoxContainer/Inputs
+onready var outputContainer = $VBoxContainer/HBoxContainer4/VBoxContainer2/outputs
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+
+
+func _on_anzahlHiddenlayer_text_changed(new_text):
+	anzahlHiddenlayer = int(new_text)
+	if int(new_text) > 0:
+		$VBoxContainer/VBoxContainer2.visible = true
+	else:
+		$VBoxContainer/VBoxContainer2.visible = false
+
+
+
+
+
+
+
+
+
+func _on_anzahlInputs_text_entered(new_text):
+	anzahlInputs = int(new_text)
+	if anzahlInputs > 9:
+		anzahlInputs = 9
+	for i in inputContainer.get_child_count():
+		if i < int(new_text):
+			inputContainer.get_child(i).visible = true
+		else:
+			inputContainer.get_child(i).visible = false
+	if int(new_text) == 0:
+		anzahlInputs = 1
+		inputContainer.get_child(0).visible = true
+
+func _on_anzahlOutputs_text_entered(new_text):
+	anzahlOutputs = int(new_text)
+	if anzahlOutputs > 9:
+		anzahlOutputs = 9
+	for i in outputContainer.get_child_count():
+		if i < int(new_text):
+			outputContainer.get_child(i).visible = true
+		else:
+			outputContainer.get_child(i).visible = false
+	if int(new_text) == 0:
+		anzahlOutputs = 1
+		inputContainer.get_child(0).visible = true
+
+	
+
+func _on_neuronenProHiddenlayer_text_entered(new_text):
+	neuronenProHiddenlayer.clear()
+	var stringNeuronenProHiddenlayer = new_text.split(",")
+	for i in stringNeuronenProHiddenlayer.size():
+		if int(stringNeuronenProHiddenlayer[i]) != null:
+			neuronenProHiddenlayer.append( int(stringNeuronenProHiddenlayer[i]))
+	print(neuronenProHiddenlayer)
+
+
+
+
+func _on_run_pressed():
+	inputs.clear()
+	outputs.clear()
+	for i in anzahlInputs:
+		inputs.append([])
+		for j in inputContainer.get_child(i).get_child_count()-1:
+			inputs[i].append( int(inputContainer.get_child(i).get_child(j+1).text))
+	for i in anzahlOutputs:
+		outputs.append([])
+		for j in outputContainer.get_child(i).get_child_count()-1:
+			outputs[i].append( int(outputContainer.get_child(i).get_child(j+1).text))
+	print(outputs)
+
+
+func _on_addOneLine_pressed():
+	
+	for i in inputContainer.get_child_count():
+		print("asdasdaa")
+		inputContainer.get_child(i).add_child( inputContainer.get_child(i).get_child(1).duplicate())
+	for i in outputContainer.get_child_count():
+		outputContainer.get_child(i).add_child( outputContainer.get_child(i).get_child(1).duplicate())
+
+
+func _on_deleteOneLine_pressed():
+	if inputContainer.get_child(0).get_child_count() > 1:
+		print(inputContainer.get_child_count()-1)
+		for i in inputContainer.get_child_count():
+			inputContainer.get_child(i).get_child(inputContainer.get_child(i).get_child_count()-1).queue_free()
+		for i in outputContainer.get_child_count():
+			outputContainer.get_child(i).get_child(outputContainer.get_child(i).get_child_count()-1).queue_free()
